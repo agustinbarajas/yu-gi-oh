1037bffe74ddcf120bdc7fdb4343924dca668ce3
Agustin Barajas <agustinbvaldivia@gmail.com>
2022-10-28 19:12:24 -0600

    Add app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+import {CardListComponent} from "./pages/card-list/card-list.component";
+import {CardDetailsComponent} from "./pages/card-details/card-details.component";
+
+const routes: Routes = [
+  {path: '', component: CardListComponent},
+  {path: 'card/:id', component: CardDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
