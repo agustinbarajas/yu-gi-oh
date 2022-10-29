11af5f156b9a1b5fdab9b6ed0720f424354ddd69
Agustin Barajas <agustinbvaldivia@gmail.com>
2022-10-28 21:57:07 -0600

    Add custom grid template columns value for auto-fit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        'auto-fit-12': 'repeat(auto-fit, minmax(12rem, 1fr))',
+      }
+    },
   },
   plugins: [],
 }
