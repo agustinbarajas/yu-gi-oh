1f4c774f50c01f69f1d2710fba284adbd8eb15bb
Agustin Barajas <agustinbvaldivia@gmail.com>
2022-10-28 19:03:01 -0600

    Create card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardListComponent } from './pages/card-list/card-list.component';
 import { CardDetailsComponent } from './pages/card-details/card-details.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardListComponent,
-    CardDetailsComponent
+    CardDetailsComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
