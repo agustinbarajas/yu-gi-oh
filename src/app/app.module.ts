1684bfac5749134a0b0fe38c9240dc44c03371c5
Agustin Barajas <agustinbvaldivia@gmail.com>
2022-12-17 13:09:34 -0700

    Add import for shared modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,30 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {HttpClientModule} from "@angular/common/http";
-import {ReactiveFormsModule} from "@angular/forms";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {CardListComponent} from './pages/card-list/card-list.component';
-import {CardDetailsComponent} from './pages/card-details/card-details.component';
-import {CardComponent} from './components/card/card.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardListComponent } from './pages/card-list/card-list.component';
+import { CardDetailsComponent } from './pages/card-details/card-details.component';
+import { CardComponent } from './components/card/card.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardListComponent,
     CardDetailsComponent,
-    CardComponent
+    CardComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
+    SharedModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
